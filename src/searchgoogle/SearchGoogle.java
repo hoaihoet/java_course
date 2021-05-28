26f1b2daa2c7afd49607da1bed4896fd82ba7ac4
Hoai Leu <hoaihoet2611@gmail.com>
2021-05-28 23:48:09 +0700

    test

--- src/searchgoogle/SearchGoogle.java
+++ src/searchgoogle/SearchGoogle.java
@@ -7,14 +7,10 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SearchGoogle {
-    public static void main(String [] args) {
-
-        WebDriver webDriver= new ChromeDriver();
-        webDriver.get("https://www.google.com");
-        
-        WebElement element = webDriver.findElement(new By.ByXPath("//*[@class='gLFyf gsfi']"));
-        element.sendKeys("test automation");
-        element.sendKeys(Keys.ENTER);
-
+    public static void main(String [] args) throws InterruptedException {
+        SetupChromeDriver setupChromeDriver = new SetupChromeDriver();
+        setupChromeDriver.setUp();
+        setupChromeDriver.enterKey();
+        setupChromeDriver.verifyResult();
     }
 }
